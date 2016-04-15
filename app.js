d5808cf35b55fcc0df0e21802b46ffa51b02c60c
Michael Mailman <mmailman@gmail.com>
2016-04-15 15:37:03 -0700

    Adding in some improvements that resulted from the pairing session. Also fixing a bug with one logs.

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 function sum(numberOne, numberTwo){
   var sum = numberOne + numberTwo;
+  var sumLogStatement = document.getElementById('sum');
+
   sumLogStatement.textContent = ('\"The sum of ' + numberOne + ' and ' + numberTwo + ' is ' + sum + '.\"');
   return sum;
 }
 
 function multiply(numberOne, numberTwo){
   var product = numberOne * numberTwo;
+  var multiplyLogStatement = document.getElementById('multiply');
   multiplyLogStatement.textContent = ('\"The product of ' + numberOne + ' and ' + numberTwo + ' is ' + product + '.\"');
   return product;
 }
@@ -15,13 +18,18 @@ function multiply(numberOne, numberTwo){
 function sumAndMultiply(numberOne, numberTwo, numberThree){
   var totalSum = sum(sum(numberOne, numberTwo), numberThree);
   var totalProduct = multiply(multiply(numberOne, numberTwo), numberThree);
-  sumAndMultiplyLogStatement.textContent = ('\"' + numberOne + ' and ' + numberTwo + ' and ' + numberThree + ' sum  to ' + totalSum + '.\"');
-  console.log('\"The numbers ' + numberOne + ' and ' + numberTwo + ' and ' + numberThree + ' have a product of ' + totalProduct + '.\"');
+  var sumAndMultiplyLogStatement1 = document.getElementById('sumAndMultiply1');
+  var sumAndMultiplyLogStatement2 = document.getElementById('sumAndMultiply2');
+
+  sumAndMultiplyLogStatement1.textContent = ('\"' + numberOne + ' and ' + numberTwo + ' and ' + numberThree + ' sum  to ' + totalSum + '.\"');
+  sumAndMultiplyLogStatement2.textContent = ('\"The numbers ' + numberOne + ' and ' + numberTwo + ' and ' + numberThree + ' have a product of ' + totalProduct + '.\"');
   return [totalSum, totalProduct];
 }
 
 function sumArray(numbersArray){
   var sum = 0;
+  var sumArrayLogStatement = document.getElementById('sumArray');
+
   for(var number = 0; number < numbersArray.length; number++){
     sum += numbersArray[number];
   }
@@ -32,6 +40,8 @@ function sumArray(numbersArray){
 
 function multiplyArray(numbersArray){
   var product = 1;
+  var multiplyArrayLogStatement = document.getElementById('multiplyArray');
+
   for(var number = 0; number < numbersArray.length; number++){
     product *= numbersArray[number];
   }
@@ -39,9 +49,3 @@ function multiplyArray(numbersArray){
   multiplyArrayLogStatement.textContent = ('\"The numbers ' + numbersArray + ' have a product of ' + product + '.\"');
   return product;
 }
-
-var sumLogStatement = document.getElementById('sum');
-var multiplyLogStatement = document.getElementById('multiply');
-var sumAndMultiplyLogStatement = document.getElementById('sumAndMultiply');
-var sumArrayLogStatement = document.getElementById('sumArray');
-var multiplyArrayLogStatement = document.getElementById('multiplyArray');
